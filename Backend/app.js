1887bce8d59556cd081d083238980a9e40ce1267
Shubhanshu004 <tiwarishubhanshu311@gmail.com>
2026-06-13 00:51:53 +0530

    added rate limiting on api routes

--- Backend/app.js
+++ Backend/app.js
@@ -3,20 +3,41 @@ const authRoutes = require('./routes/auth.routes')
 const CoursesRoutes = require('./routes/courses.routes')
 const messageRoutes = require('./routes/messages.routes')
 const sessionRoutes = require('./routes/sessions.routes')
+const rateLimit = require('express-rate-limit')
+const cors = require('cors')
 
 const app = express()
 
 //middleware
 app.use(express.json());
 
-const cors = require('cors')
+
+
 app.use(cors())
 
+//rate limiter
+const apilimiter = rateLimit({
+    windowMs: 15*60*1000 , 
+    max: 100,
+    message: {error: 'Too many requests , please try again later'}
+})
+const chatLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 mins
+    max: 15, // strictly limit only 15 messages per 15 minutes to save AI costs
+    message: { error: 'You have reached the chat limit. Please wait 15 minutes.' }
+});
+
+
+
+
+
+
+
 //routes
-app.use('/auth' , authRoutes)
-app.use('/courses' ,CoursesRoutes )
-app.use('/', messageRoutes)
-app.use('/', sessionRoutes)
+app.use('/auth' ,apilimiter,  authRoutes)
+app.use('/courses' ,apilimiter ,CoursesRoutes )
+app.use('/',chatLimiter , messageRoutes)
+app.use('/',apilimiter , sessionRoutes)
 
 
 module.exports = app
